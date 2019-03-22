66a5a9e319f2e121f4f4207ac580cdb75efbfb11
vjawa <vjawa@nvidia.com>
2019-03-22 19:41:33 +0000

    added code for conversion

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# apt update && apt upgrade
+# apt install wget
+# apt-get install default-jre
+cd ~
+
+# get and install pyspark
+#wget http://apache.mirrors.pair.com/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz
+#tar -xzf spark-2.4.0-bin-hadoop2.7.tgz
+#mv spark-2.4.0-bin-hadoop2.7 /opt/spark-2.4.0
+#ln -s /opt/spark-2.4.0 /opt/spark̀
+
+# add spark context to your bashrc
+# echo "export SPARK_HOME=/opt/spark" >>  ~/.bashrc
+# echo "export PATH=$SPARK_HOME/bin:$PATH">> ~/.bashrc
+
+# used to find spark context in jupyter
+# conda install -c conda-forge findspark 
