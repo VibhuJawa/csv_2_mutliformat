b24c8a4151ca9344be64e804e4a26423ca8ae6cf
vibhujawa <vibhujawa@gmail.com>
2019-07-17 14:11:55 -0700

    added asrvo reader+setup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-apt update && apt upgrade
-apt install wget
-apt-get install default-jre
+# sudo apt update && apt upgrade
+# sudo apt install wget
+# sudo apt-get install default-jre
 cd ~
 
 # get and install pyspark
-wget http://apache.mirrors.pair.com/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz
-tar -xzf spark-2.4.0-bin-hadoop2.7.tgz
-mv spark-2.4.0-bin-hadoop2.7 /opt/spark-2.4.0
-ln -s /opt/spark-2.4.0 /opt/spark̀
+# wget http://apache.claz.org/spark/spark-2.4.3/spark-2.4.3-bin-hadoop2.7.tgz 
+# http://apache.mirrors.pair.com/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz
+# tar -xzf spark-2.4.3-bin-hadoop2.7.tgz
+sudo mv spark-2.4.3-bin-hadoop2.7 /opt/spark-2.4.3
+sudo ln -s /opt/spark-2.4.3 /opt/spark̀
 
 # add spark context to your bashrc
 echo "export SPARK_HOME=/opt/spark" >>  ~/.bashrc
